46d67d674a6d6ad7f6989f602a860742ba541163
Giovane Cana Verde <giovane.cverde@gmail.com>
2025-05-22 20:38:19 -0300

    start

---
+++ script.js
@@ -0,0 +1,15 @@
+let character = 'Hello';
+
+console.log(character);
+
+character = 'World';
+
+console.log(character);
+
+let secondCharacter;
+
+console.log(secondCharacter);
+
+secondCharacter = 'Test';
+
+console.log(secondCharacter);
