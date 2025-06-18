3a259e925f4b13f2575e0e1ca99cbca303070992
Giovane Cana Verde <giovane.cverde@gmail.com>
2025-06-17 23:46:25 -0300

    addTwoNumbers

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function padRow(name) {
     return name;
 }
 
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+const sum = addTwoNumbers(5, 10);
+console.log(sum)
+
+const call = padRow();
+console.log(call);
+
 for (let i = 0; i < count; i = i + 1) {
     rows.push(character.repeat(i + 1));
 }
