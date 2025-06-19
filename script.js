977c06e44d99a6baa2bf28db94285d1e3e3c02c4
Giovane Cana Verde <giovane.cverde@gmail.com>
2025-06-18 22:50:32 -0300

    forming the pyramid

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-    return character.repeat(rowNumber); 
+    const spaces = " ".repeat(rowCount - rowNumber);
+    const blocks = character.repeat(2 * rowNumber - 1);
+    return spaces + blocks + spaces; 
 }
 
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i + 1));
+    rows.push(padRow(i + 1, count));
 }
 
 let result = "";
